218fda5a415fda056e7e2542a02b2b31dd07a43c
eirondiaz <eiron.diaz-ext@ab-inbev.com>
2025-03-11 18:01:49 -0400

    feat: logo darkmode disabled

--- app/page.tsx
+++ app/page.tsx
@@ -38,7 +38,7 @@ export default function Home() {
     <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
       <div onMouseEnter={handleMouseEnter}>
         <Image
-          className="dark:invert"
+          className="dark"
           src="/video-mordida.gif"
           alt="Next.js logo"
           width={160}
